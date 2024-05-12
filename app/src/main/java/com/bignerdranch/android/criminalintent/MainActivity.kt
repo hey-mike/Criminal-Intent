ba544bbb4ad7ae6835d8a5c5feb6ae1b6ead81e9
hey-mike <michael.luo0801@gmail.com>
2024-05-12 17:51:24 +1000

    add recycle view

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bignerdranch.android.criminalintent.databinding.ActivityMainBinding
+
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        setupWindowInsets()
+    }
+
+    private fun setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
